b7df7ca128eb59bd47e33d0dc4eab0cf7f0d6e45
Kento Okura <kentookura@gmail.com>
2023-11-11 00:31:13 +0000

    first prototype

--- flake.nix
+++ flake.nix
@@ -1,67 +1,35 @@
 {
   inputs = {
-    opam-nix.url = "github:tweag/opam-nix";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.follows = "opam-nix/nixpkgs";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    opam-repository =
-      {
-        url = "github:ocaml/opam-repository";
-        flake = false;
-      };
-    forester = {
-      url = "sourcehut:~jonsterling/ocaml-forester";
-      flake = false;
-    };
-    asai = {
-      url = "github:RedPRL/asai";
-      flake = false;
-    };
   };
   outputs =
     { self
     , flake-utils
     , rust-overlay
-    , opam-repository
-    , opam-nix
     , nixpkgs
-    , asai
-    , forester
     }@inputs:
     # Don't forget to put the package name instead of `throw':
     flake-utils.lib.eachDefaultSystem
       (system:
       let
-        my-python-packages = ps: with ps; [
-          websockets
-          # other python packages
-        ];
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-        on = opam-nix.lib.${system};
-        overlay = final: prev:
-          { };
-        devPackagesQuery = {
-          asai = "*";
-        };
-        query = devPackagesQuery // {
-          ocaml-base-compiler = "*";
-        };
-        scope = on.buildOpamProject' { repos = [ "${opam-repository}" asai ]; } forester query;
-        scope' = scope.overrideScope' overlay;
-        main = scope'."forester";
-        devPackages = builtins.attrValues
-          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
       in
+      with pkgs;
       {
-        legacyPackages = scope.overrideScope' overlay;
+        packages.default = pkgs.rustPlatform.buildRustPackage {
 
-        packages.default = self.legacyPackages.${system}."forester";
-        devShell = with opam-nix.inputs.nixpkgs.legacyPackages.${system};
-          mkShell {
+          pname = "forest-server";
+          version = "0.0.1";
+          src = ./.;
+          cargoLock = { lockFile = ./Cargo.lock; };
+          buildInputs = [ ];
+        };
+        devShell =
+          pkgs.mkShell {
             buildInputs = with pkgs; [
-              (python3.withPackages my-python-packages)
-              self.legacyPackages.${system}."forester"
               rust-analyzer-unwrapped
               (rust-bin.beta.latest.default.override { extensions = [ "rust-src" "rust-analyzer-preview" ]; })
             ];
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,243 @@
-use std::{
-    sync::{
-        atomic::{AtomicUsize, Ordering},
-        Arc,
-    },
-    time::Instant,
-};
-
-use actix::*;
 use actix_files::{Files, NamedFile};
-use actix_web::{
-    middleware::Logger, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
+use actix_rt::Arbiter;
+use actix_web::{middleware::Logger, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_ws::{Message, Session};
+use async_priority_channel as priority;
+use futures::stream::{FuturesUnordered, StreamExt};
+use log::info;
+use std::ffi::OsStr;
+use std::ffi::OsString;
+use std::{
+    sync::Arc,
+    time::{Duration, Instant},
 };
-use actix_web_actors::ws;
+use tokio;
+//use tokio::process::Command;
+use tokio::sync::Mutex;
+use watchexec::command::Command;
+use watchexec::{
+    self,
+    action::{Action, Outcome},
+    config::{InitConfig, RuntimeConfig},
+    error::ReconfigError,
+    event::{Event, Priority, Tag},
+    Watchexec,
+};
+use watchexec_filterer_globset::GlobsetFilterer;
+use watchexec_signals::Signal;
 
-mod reloader;
-mod reloader_session;
+#[derive(Clone)]
+struct Reload {
+    inner: Arc<Mutex<ReloadInner>>,
+}
+
+struct ReloadInner {
+    sessions: Vec<Session>,
+}
+
+impl Reload {
+    fn new() -> Self {
+        Reload {
+            inner: Arc::new(Mutex::new(ReloadInner {
+                sessions: Vec::new(),
+            })),
+        }
+    }
+
+    async fn insert(&self, session: Session) {
+        self.inner.lock().await.sessions.push(session);
+    }
+
+    async fn send(&self, msg: String) {
+        let mut inner = self.inner.lock().await;
+        let mut unordered = FuturesUnordered::new();
+
+        for mut session in inner.sessions.drain(..) {
+            let msg = msg.clone();
+            unordered.push(async move {
+                let res = session.text(msg).await;
+                res.map(|_| session).map_err(|_| info!("Dropping session"))
+            });
+        }
+
+        while let Some(res) = unordered.next().await {
+            if let Ok(session) = res {
+                inner.sessions.push(session);
+            }
+        }
+    }
+}
+
+async fn ws(
+    req: HttpRequest,
+    body: web::Payload,
+    reloader: web::Data<Reload>,
+) -> Result<HttpResponse, actix_web::Error> {
+    let (response, mut session, mut stream) = actix_ws::handle(&req, body)?;
+
+    reloader.insert(session.clone()).await;
+    info!("Inserted session");
+
+    let alive = Arc::new(Mutex::new(Instant::now()));
+
+    let mut session2 = session.clone();
+    let alive2 = alive.clone();
+
+    actix_rt::spawn(async move {
+        let mut interval = actix_rt::time::interval(Duration::from_secs(5));
+        loop {
+            interval.tick().await;
+            if session2.ping(b"").await.is_err() {
+                break;
+            }
+
+            if Instant::now().duration_since(*alive2.lock().await) > Duration::from_secs(10) {
+                let _ = session2.close(None).await;
+                break;
+            }
+        }
+    });
+
+    actix_rt::spawn(async move {
+        while let Some(Ok(msg)) = stream.next().await {
+            match msg {
+                Message::Ping(bytes) => {
+                    if session.pong(&bytes).await.is_err() {
+                        return;
+                    }
+                }
+                Message::Text(s) => {
+                    info!("Relaying text, {}", s);
+                    let s: &str = s.as_ref();
+                    reloader.send(s.into()).await;
+                }
+                Message::Close(reason) => {
+                    let _ = session.close(reason).await;
+                    info!("Got close, bailing");
+                    return;
+                }
+                Message::Continuation(_) => {
+                    let _ = session.close(None).await;
+                    info!("Got continuation, bailing");
+                    return;
+                }
+                Message::Pong(_) => {
+                    *alive.lock().await = Instant::now();
+                }
+                _ => (),
+            };
+        }
+        let _ = session.close(None).await;
+    });
+    info!("Spawned");
+
+    Ok(response)
+}
 
 async fn index() -> impl Responder {
-    NamedFile::open_async("./static/index.html").await.unwrap()
+    NamedFile::open_async("./output/index.xml").await.unwrap()
 }
 
-/// Entry point for our websocket route
-async fn reload_route(
-    req: HttpRequest,
-    stream: web::Payload,
-    srv: web::Data<Addr<reloader::ReloadServer>>,
-) -> Result<HttpResponse, Error> {
-    ws::start(
-        reloader_session::ReloaderSession {
-            id: 0,
-            hb: Instant::now(),
-            //room: "main".to_owned(),
-            //name: None,
-            addr: srv.get_ref().clone(),
-        },
-        &req,
-        stream,
+pub async fn filt(filters: &[&str], ignores: &[&str], extensions: &[&str]) -> GlobsetFilterer {
+    let origin = tokio::fs::canonicalize(".").await.unwrap();
+    GlobsetFilterer::new(
+        origin,
+        filters.iter().map(|s| ((*s).to_string(), None)),
+        ignores.iter().map(|s| ((*s).to_string(), None)),
+        vec![],
+        extensions.iter().map(OsString::from),
     )
-}
-
-/// Displays state
-async fn get_count(count: web::Data<AtomicUsize>) -> impl Responder {
-    let current_count = count.load(Ordering::SeqCst);
-    format!("Visitors: {current_count}")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-
-    // set up applications state
-    // keep a count of the number of visitors
-    let app_state = Arc::new(AtomicUsize::new(0));
-
-    // start chat server actor
-    let server = reloader::ReloadServer::new().start();
-
-    log::info!("starting HTTP server at http://localhost:8080");
-
-    HttpServer::new(move || {
-        App::new()
-            .app_data(web::Data::from(app_state.clone()))
-            .app_data(web::Data::new(server.clone()))
-            .service(web::resource("/").to(index))
-            .route("/count", web::get().to(get_count))
-            .route("/reload", web::get().to(reload_route))
-            .service(Files::new("/static", "./static"))
-            .wrap(Logger::default())
-    })
-    .workers(2)
-    .bind(("127.0.0.1", 8080))?
-    .run()
     .await
+    .expect("making filterer")
+}
+
+//#[actix_rt::main]
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    std::env::set_var("RUST_LOG", "info");
+    pretty_env_logger::init();
+
+    let reloader = Reload::new();
+    let ch = reloader.clone();
+    let init = InitConfig::default();
+    let mut runtime = RuntimeConfig::default();
+    runtime
+        .pathset(["trees", "theme", "assets"])
+        .filterer(Arc::new(
+            filt(&[], &[], &["tree", "js", "css", "xsl"]).await,
+        ))
+        .command(
+            Command::Exec {
+                prog: "forester".into(),
+                args: vec![
+                    "build".to_string(),
+                    "--dev".to_string(),
+                    "--root".to_string(),
+                    "index".to_string(),
+                    "trees".to_string(),
+                ],
+            }, //("forester")
+               //    .arg("build")
+               //    .arg("--dev")
+               //    .arg("--root")
+               //    .arg("index")
+               //    .arg("trees")
+               //    .status()
+               //    .await
+               //    .unwrap();
+        )
+        .on_action(move |action: Action| {
+            let ch = ch.clone();
+            let evs = action.events.clone();
+            async move {
+                let sigs = action
+                    .events
+                    .iter()
+                    .flat_map(Event::signals)
+                    .collect::<Vec<_>>();
+                if sigs.iter().any(|sig| sig == &Signal::Interrupt) {
+                    action.outcome(Outcome::Exit);
+                }
+
+                for event in evs.iter() {
+                    log::info!("detected changes");
+                    //Command::Exec {
+                    //  prog: "forester".into(),
+                    //};
+                    //("forester")
+                    //    .arg("build")
+                    //    .arg("--dev")
+                    //    .arg("--root")
+                    //    .arg("index")
+                    //    .arg("trees")
+                    //    .status()
+                    //    .await
+                    //    .unwrap();
+                    ch.send("reload".into()).await;
+                }
+                Ok::<(), ReconfigError>(())
+            }
+        });
+    let server = HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .app_data(web::Data::new(reloader.clone()))
+            .route("/", web::get().to(index))
+            .route("/reload", web::get().to(ws))
+            .service(Files::new("/", "./output"))
+    });
+    //.bind("127.0.0.1:8080");
+
+    tokio::spawn(
+        server
+            .bind("127.0.0.1:8080")
+            .expect("Failed to bind addr")
+            .run(),
+    );
+
+    let we = Watchexec::new(init, runtime.clone()).unwrap();
+    tokio::spawn(we.main()).await?.unwrap();
+    //tokio::spawn(we.main()).await?.unwrap();
+
+    Ok(())
 }
