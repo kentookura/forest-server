e3414c25bd1f851a729ff896e03924a75966eab6
Kento Okura <kentookura@gmail.com>
2023-12-04 14:06:47 +0100

    :bug: Notify user when arguments are missing

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,8 @@ async fn main() -> miette::Result<()> {
             let opts = sub_matches
                 .get_one::<String>("opts")
                 .map(|s| s.as_str())
-                .unwrap();
+                .unwrap()
+                .expect("make sure to pass arguments to forester like so: forest watch -- \"build --dev --index root trees/\"" )
 
             let Some(port) = sub_matches.get_one::<u16>("port") else {
                 todo!()
