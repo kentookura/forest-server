42c4a913d143c309baee08a3910fbade6ac0b85b
Kento Okura <kentookura@gmail.com>
2023-12-04 12:21:28 +0100

    :bookmark: Change binary name to forest

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "forest-server";
-          version = "0.1.0";
+          version = "0.2.0";
           src = ./.;
           cargoLock = { lockFile = ./Cargo.lock; };
           nativeBuildInputs = with pkgs;[ pkg-config ];
