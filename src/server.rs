66aecb0a6ea10d4537bfdc24ec707c134f62390d
Kento Okura <kentookura@gmail.com>
2023-12-05 10:59:47 +0100

    :sparkles: Reload only when using `watch` subcommand.

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,10 @@
 use axum::{
     extract::State,
-    response::sse::{Event, Sse},
+    http::header,
+    response::{
+        sse::{Event, Sse},
+        IntoResponse,
+    },
     routing::{get, get_service},
     Router,
 };
@@ -29,6 +33,7 @@ pub async fn server(port: u16, rx: Option<Receiver<Event>>) {
         let state = ServerState { rx };
         let router = Router::new()
             .route("/reload", get(sse_handler))
+            .route("/reload.js", get(reloader))
             .nest_service("/", get_service(ServeDir::new("output")))
             .layer(TraceLayer::new_for_http())
             .with_state(state);
@@ -37,13 +42,16 @@ pub async fn server(port: u16, rx: Option<Receiver<Event>>) {
             .await
             .expect("failed to start server")
     } else {
-        let router = Router::new().nest_service("/", get_service(ServeDir::new("output")));
+        let router = Router::new()
+            .nest_service("/", get_service(ServeDir::new("output")))
+            .route("/reload.js", get(notify_no_sse_support));
         axum::serve(listener, router.into_make_service())
             .await
             .expect("failed to start server")
     }
 }
 
+#[axum::debug_handler]
 async fn sse_handler(State(state): State<ServerState>) -> Sse<BroadcastStream<Event>> {
     let rx: Receiver<Event> = state.rx.lock().unwrap().resubscribe();
     let stream = BroadcastStream::new(rx);
@@ -52,4 +60,24 @@ async fn sse_handler(State(state): State<ServerState>) -> Sse<BroadcastStream<Ev
             .interval(Duration::from_secs(1))
             .text("keep-alive-text"),
     )
+}
+
+async fn reloader() -> impl IntoResponse {
+    let content = "
+const evtSource = new EventSource(\"reload\");
+evtSource.onmessage = (event) => {
+    if (event.data == \"reload\") {
+      location.reload();
+    } else {
+      console.log(event.data);
+    }
+};";
+
+    ([(header::CONTENT_TYPE, "text/javascript")], content)
+}
+
+async fn notify_no_sse_support() -> impl IntoResponse {
+    let message = "Hot-Reload feature not enabled";
+    let content = format!("console.log(\"{}\")", message);
+    ([(header::CONTENT_TYPE, "text/javascript")], content)
 }
